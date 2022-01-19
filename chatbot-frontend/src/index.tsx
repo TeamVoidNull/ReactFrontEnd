09bc7bc22997e98a500b5327129cc323f19ade3f
racheljiezhang <racheljiezhang@gmail.com>
2022-01-19 16:54:52 -0500

    Changed to tempcontainer for now

--- chatbot-frontend/src/index.tsx
+++ chatbot-frontend/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@progress/kendo-react-conversational-ui'
 
 ReactDOM.render(
   <React.StrictMode>
